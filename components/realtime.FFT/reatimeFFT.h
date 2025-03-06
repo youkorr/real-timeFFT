1989398f25ed67a0ac747b79913b3ef7412d3f77
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-07 00:24:34 +0100

    Update reatimeFFT.h

--- components/realtime.FFT/reatimeFFT.h
+++ components/realtime.FFT/reatimeFFT.h
@@ -6,25 +6,26 @@
 #include <cmath>
 #include "esphome.h"
 #include "esphome/components/lvgl/lvgl.h"
+#include "esphome/components/sensor/sensor.h"
 
-class RealtimeFFT : public Component, public Sensor {
+class RealtimeFFT : public Component, public Parented<Sensor> {
 public:
-    RealtimeFFT(int fftSize = 1024);
-    
-    void processAudioData(const std::vector<float>& audioInput);
-    void update_lvgl_chart();
-
-protected:
     void setup() override;
     void loop() override;
+    
+    void processAudioData(const std::vector<float>& audioInput);
+    void set_chart(lv_obj_t *chart) { chart_ = chart; }
 
 private:
-    int m_fftSize;
-    std::vector<std::complex<float>> m_complexBuffer;
-    std::vector<float> m_magnitudeSpectrum;
+    static const int FFT_SIZE = 1024;
+    std::vector<std::complex<float>> complexBuffer;
+    std::vector<float> magnitudeSpectrum;
+    lv_obj_t *chart_ = nullptr;
 
     void cooleyTukeyFFT(std::vector<std::complex<float>>& data);
     void bitReversalPermutation(std::vector<std::complex<float>>& data);
+    void update_chart();
+    void update_sensor();
 };
 
 #endif
