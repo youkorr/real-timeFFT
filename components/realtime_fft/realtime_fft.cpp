86c351ca833e0bbb60ccdcabe209d19e57f26e0d
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-07 18:59:39 +0100

    Update realtime_fft.cpp

--- components/realtime_fft/realtime_fft.cpp
+++ components/realtime_fft/realtime_fft.cpp
@@ -7,7 +7,7 @@ namespace realtime_fft {
 static const char *TAG = "realtime_fft";
 
 void RealtimeFFTComponent::setup() {
-  ESP_LOGD(TAG, "Setting up Realtime FFT with ESP-IDF...");
+  ESP_LOGD(TAG, "Setting up Realtime FFT...");
   
   if (this->i2s_audio_ == nullptr) {
     ESP_LOGE(TAG, "I2S Audio component not set!");
@@ -15,76 +15,46 @@ void RealtimeFFTComponent::setup() {
     return;
   }
   
-  #ifdef USE_ESP_IDF
   // Allocate buffers
   this->input_buffer_ = new float[this->fft_size_];
   this->fft_output_ = new float[this->fft_size_ / 2];
   this->window_ = new float[this->fft_size_];
-  this->fft_data_ = new dsp_complex_t[this->fft_size_];
-  
-  if (!init_dsp()) {
-    ESP_LOGE(TAG, "DSP initialization failed");
-    this->mark_failed();
-    return;
-  }
+  this->real_ = new float[this->fft_size_];
+  this->imag_ = new float[this->fft_size_];
   
   // Create Hanning window
   for (int i = 0; i < this->fft_size_; i++) {
     this->window_[i] = 0.5f * (1.0f - cosf(2.0f * M_PI * i / (this->fft_size_ - 1)));
   }
-  #endif
   
   ESP_LOGD(TAG, "FFT initialized with sample rate %d Hz and FFT size %d", this->sample_rate_, this->fft_size_);
 }
-
-#ifdef USE_ESP_IDF
-bool RealtimeFFTComponent::init_dsp() {
-  esp_err_t ret = esp_dsp_init();
-  if (ret != ESP_OK) {
-    ESP_LOGE(TAG, "Failed to initialize DSP: %d", ret);
-    return false;
-  }
-  
-  ret = esp_dsp_fft2r_init_c2c(this->fft_size_, &this->dsp_);
-  if (ret != ESP_OK) {
-    ESP_LOGE(TAG, "Failed to initialize FFT: %d", ret);
-    return false;
-  }
-  
-  return true;
-}
-#endif
 
 void RealtimeFFTComponent::loop() {
   if (this->i2s_audio_ == nullptr) {
     return;
   }
   
-  #ifdef USE_ESP_IDF
   this->process_audio();
-  #endif
 }
 
-#ifdef USE_ESP_IDF
 void RealtimeFFTComponent::process_audio() {
   // Get audio samples from I2S
   size_t bytes_read;
   i2s_read(I2S_NUM_0, this->input_buffer_, this->fft_size_ * sizeof(float), &bytes_read, portMAX_DELAY);
   
-  // Apply window function
+  // Copy to real buffer and apply window
   for (int i = 0; i < this->fft_size_; i++) {
-    this->fft_data_[i].re = this->input_buffer_[i] * this->window_[i];
-    this->fft_data_[i].im = 0;
+    this->real_[i] = this->input_buffer_[i] * this->window_[i];
+    this->imag_[i] = 0;
   }
   
   // Perform FFT
-  esp_dsp_fft2r_c2c(this->dsp_, this->fft_data_, this->fft_size_);
+  this->fft(this->real_, this->imag_, this->fft_size_);
   
   // Calculate magnitudes
   for (int i = 0; i < this->fft_size_ / 2; i++) {
-    float real = this->fft_data_[i].re;
-    float imag = this->fft_data_[i].im;
-    this->fft_output_[i] = sqrtf(real * real + imag * imag);
+    this->fft_output_[i] = sqrtf(this->real_[i] * this->real_[i] + this->imag_[i] * this->imag_[i]);
   }
   
   // Publish max value
@@ -96,14 +66,60 @@ void RealtimeFFTComponent::process_audio() {
   }
   this->publish_state(max_value);
 }
-#endif
+
+void RealtimeFFTComponent::fft(float *real, float *imag, int n) {
+  int i, j, k, m;
+  float theta, wpr, wpi, wr, wi, tempr, tempi;
+  
+  // Bit-reverse
+  j = 0;
+  for (i = 0; i < n - 1; i++) {
+    if (i < j) {
+      tempr = real[j];
+      real[j] = real[i];
+      real[i] = tempr;
+      tempi = imag[j];
+      imag[j] = imag[i];
+      imag[i] = tempi;
+    }
+    k = n / 2;
+    while (k <= j) {
+      j -= k;
+      k /= 2;
+    }
+    j += k;
+  }
+  
+  // Danielson-Lanczos
+  m = 2;
+  while (m <= n) {
+    theta = -2.0f * M_PI / m;
+    wpr = cosf(theta);
+    wpi = sinf(theta);
+    wr = 1.0f;
+    wi = 0.0f;
+    
+    for (j = 0; j < m / 2; j++) {
+      for (i = j; i < n; i += m) {
+        k = i + m / 2;
+        tempr = wr * real[k] - wi * imag[k];
+        tempi = wr * imag[k] + wi * real[k];
+        real[k] = real[i] - tempr;
+        imag[k] = imag[i] - tempi;
+        real[i] += tempr;
+        imag[i] += tempi;
+      }
+      wr = (tempr = wr) * wpr - wi * wpi;
+      wi = wi * wpr + tempr * wpi;
+    }
+    m *= 2;
+  }
+}
 
 float RealtimeFFTComponent::get_fft_value(int bin) {
-  #ifdef USE_ESP_IDF
   if (bin >= 0 && bin < this->fft_size_ / 2) {
     return this->fft_output_[bin];
   }
-  #endif
   return 0.0f;
 }
 
@@ -115,11 +131,7 @@ float RealtimeFFTComponent::get_frequency(int bin) {
 }
 
 float *RealtimeFFTComponent::get_spectrum_data() {
-  #ifdef USE_ESP_IDF
   return this->fft_output_;
-  #else
-  return nullptr;
-  #endif
 }
 
 }  // namespace realtime_fft
