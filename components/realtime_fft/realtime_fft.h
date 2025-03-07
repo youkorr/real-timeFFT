8da31c728f05eb2c36bcbcac146a3f81e3445f09
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-07 18:42:54 +0100

    Update realtime_fft.h

--- components/realtime_fft/realtime_fft.h
+++ components/realtime_fft/realtime_fft.h
@@ -3,7 +3,8 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2s_audio/i2s_audio.h"
-
+#include "driver/i2s.h"
+#include "esp_dsp.h"
 
 namespace esphome {
 namespace realtime_fft {
@@ -17,7 +18,6 @@ class RealtimeFFTComponent : public Component, public sensor::Sensor {
   void set_fft_size(int fft_size) { this->fft_size_ = fft_size; }
   void set_i2s_audio_id(i2s_audio::I2SAudioComponent *i2s_audio) { this->i2s_audio_ = i2s_audio; }
   
-  // Méthodes pour accéder aux données FFT
   float get_fft_value(int bin);
   float get_frequency(int bin);
   float *get_spectrum_data();
@@ -29,19 +29,14 @@ class RealtimeFFTComponent : public Component, public sensor::Sensor {
   int fft_size_{1024};
   i2s_audio::I2SAudioComponent *i2s_audio_{nullptr};
   
-  // Buffer pour les données audio
-  int16_t *audio_buffer_{nullptr};
-  size_t buffer_size_{0};
+  float *input_buffer_{nullptr};
+  float *fft_output_{nullptr};
+  float *window_{nullptr};
+  dsp_complex_t *fft_data_{nullptr};
+  esp_dsp_iface_handle_t dsp_;
   
-  // Tableaux pour le calcul FFT
-  double *real_values_{nullptr};
-  double *imag_values_{nullptr};
-  float *spectrum_data_{nullptr};
-  float *frequency_bins_{nullptr};
-  
-  // Objet FFT
-  ArduinoFFT<double> *fft_{nullptr};
+  bool init_dsp();
+  void process_audio();
 };
-
 }  // namespace realtime_fft
 }  // namespace esphome
