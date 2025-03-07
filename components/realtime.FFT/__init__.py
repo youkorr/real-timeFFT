b2e8ba8c245cbe47773b993d201f90153b5b2acb
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-07 16:57:56 +0100

    Update __init__.py

--- components/realtime.FFT/__init__.py
+++ components/realtime.FFT/__init__.py
@@ -1,12 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome.components import sensor, i2s_audio
 from esphome.const import CONF_ID
 
 # Définir les constantes pour le composant
 CONF_SAMPLE_RATE = "sample_rate"
 CONF_FFT_SIZE = "fft_size"
-CONF_AUDIO_PIN = "audio_pin"
+CONF_I2S_AUDIO_ID = "i2s_audio_id"
 
 # Namespace pour le composant
 realtime_fft_ns = cg.esphome_ns.namespace("realtime_fft")
@@ -15,10 +15,10 @@ RealtimeFFTComponent = realtime_fft_ns.class_("RealtimeFFTComponent", cg.Compone
 # Configuration schema
 CONFIG_SCHEMA = sensor.sensor_schema().extend({
     cv.GenerateID(): cv.declare_id(RealtimeFFTComponent),
-    cv.Required(CONF_AUDIO_PIN): cv.pin,
+    cv.Required(CONF_I2S_AUDIO_ID): cv.use_id(i2s_audio.I2SAudioComponent),
     cv.Optional(CONF_SAMPLE_RATE, default=44100): cv.positive_int,
     cv.Optional(CONF_FFT_SIZE, default=1024): cv.positive_int,
-})
+}).extend(cv.COMPONENT_SCHEMA)
 
 # Fonction pour générer le code C++
 async def to_code(config):
@@ -26,6 +26,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     
+    i2s_audio_var = await cg.get_variable(config[CONF_I2S_AUDIO_ID])
+    cg.add(var.set_i2s_audio_id(i2s_audio_var))
     cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
     cg.add(var.set_fft_size(config[CONF_FFT_SIZE]))
-    cg.add(var.set_audio_pin(config[CONF_AUDIO_PIN]))
