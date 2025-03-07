261dbb96dfe33c0ba1da179d06b4e35e8ad62091
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-07 16:52:14 +0100

    Update reatimeFFT.h

--- components/realtime.FFT/reatimeFFT.h
+++ components/realtime.FFT/reatimeFFT.h
@@ -1,37 +1,42 @@
-#ifndef REALTIME_FFT_H
-#define REALTIME_FFT_H
+#pragma once
 
-#include <vector>
-#include <complex>
-#include <cmath>
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "arduinoFFT.h"
 
-class RealtimeFFT {
-public:
-    RealtimeFFT(int fftSize = 1024);
-    
-    // Process audio data and compute FFT
-    void processAudioData(const std::vector<float>& audioInput);
-    
-    // Get magnitude spectrum
-    std::vector<float> getMagnitudeSpectrum() const;
-    
-    // Get frequency bins
-    std::vector<float> getFrequencyBins() const;
-    
-    // Compute peak frequencies
-    std::vector<float> findPeakFrequencies(int numPeaks = 5) const;
+namespace esphome {
+namespace realtime_fft {
 
-private:
-    int m_fftSize;
-    std::vector<std::complex<float>> m_complexBuffer;
-    std::vector<float> m_magnitudeSpectrum;
-    std::vector<float> m_frequencyBins;
-
-    // Perform Cooley-Tukey FFT
-    void cooleyTukeyFFT(std::vector<std::complex<float>>& data);
-    
-    // Bit reversal for FFT
-    void bitReversalPermutation(std::vector<std::complex<float>>& data);
+class RealtimeFFTComponent : public Component, public sensor::Sensor {
+ public:
+  void setup() override;
+  void loop() override;
+  
+  void set_sample_rate(int sample_rate) { this->sample_rate_ = sample_rate; }
+  void set_fft_size(int fft_size) { this->fft_size_ = fft_size; }
+  void set_audio_pin(int audio_pin) { this->audio_pin_ = audio_pin; }
+  
+  // Méthodes pour accéder aux données FFT
+  float get_fft_value(int bin);
+  float get_frequency(int bin);
+  float *get_spectrum_data();
+  
+  float get_setup_priority() const override { return setup_priority::DATA; }
+  
+ protected:
+  int sample_rate_{44100};
+  int fft_size_{1024};
+  int audio_pin_{};
+  
+  // Tableaux pour le calcul FFT
+  double *real_values_{nullptr};
+  double *imag_values_{nullptr};
+  float *spectrum_data_{nullptr};
+  float *frequency_bins_{nullptr};
+  
+  // Objet FFT
+  ArduinoFFT<double> *fft_{nullptr};
 };
 
-#endif // REALTIME_FFT_H
+}  // namespace realtime_fft
+}  // namespace esphome
